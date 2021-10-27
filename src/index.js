ee7388794c70ef4465fecfe4e1ebdd3aca658432
Ronald Burden <developer.rburden@gmail.com>
2021-10-27 01:16:46 -0400

    adding inbox section functionality

--- src/index.js
+++ src/index.js
@@ -4,9 +4,38 @@ import Todo from "./components/Todo";
 import Project from "./components/Project";
 
 const projCollapse = document.getElementById("project-collapse");
+const addTask = document.getElementById("add-task");
 const btnToggler = document.querySelector(".btn-toggle");
 const biPlus = document.querySelector(".project-data .bi-plus");
-console.log(biPlus);
+const sidebar = document.querySelector(".sidebar");
+const display = document.querySelector(".display");
+const modal = document.querySelector(".modal");
+const addTaskForm = document.getElementById("add-task-form");
+const inboxAddTaskBtn = document.getElementById("inbox-add-task");
+const inputBtnAddTask = document.getElementById("input-btn-add-task");
+const formText = document.getElementById("input-text");
+console.log(inboxAddTaskBtn);
+// console.log({ sidebar, display });
+
+// Input Form Add Task button Event Listener
+inputBtnAddTask.addEventListener("click", function (e) {
+  console.log(formText.value);
+  console.log("clicking");
+  e.preventDefault();
+});
+
+// Input Add Task Button Event Listener
+inboxAddTaskBtn.addEventListener("click", function (e) {
+  addTaskForm.classList.toggle("hide");
+  e.target.classList.toggle("hide");
+  console.log(e.target);
+});
+
+// Add new task event listeners
+addTask.addEventListener("click", function () {
+  modal.style.display = "block";
+  console.log("Add a new task");
+});
 
 // Show and hide projects
 btnToggler.addEventListener("click", function (e) {
